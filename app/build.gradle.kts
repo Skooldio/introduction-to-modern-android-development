2fc57679387ab493731f2ac49f74c9a2cc5471c8
Akexorcist <akexorcist@gmail.com>
2024-08-23 03:21:58 +0700

    Add Coil library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation(libs.gson)
     implementation(libs.arrow.core)
 
-    // TODO 6: Add Coil library
+    implementation(libs.coil)
 
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.android)
