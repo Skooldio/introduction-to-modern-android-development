9696aab6c1bd84fd3181e3de6d63902313375e78
Akexorcist <akexorcist@gmail.com>
2024-08-23 21:18:30 +0700

    Add GitHub API path

--- app/src/main/java/com/skooldio/githubviewer/data/GitHubApi.kt
+++ app/src/main/java/com/skooldio/githubviewer/data/GitHubApi.kt
@@ -2,23 +2,21 @@ package com.skooldio.githubviewer.data
 
 import com.skooldio.githubviewer.data.response.RepositoryResponse
 import com.skooldio.githubviewer.data.response.UserResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GitHubApi {
-    // TODO 11: Add API path to get user information
     // https://api.github.com/users/android
     // users/{user}
-
+    @GET("users/{user}")
     suspend fun getUser(
-        // TODO 11: Refer the variable to API path
-        user: String
+        @Path("user") user: String
     ): UserResponse
 
-    // TODO 11: Add API path to get user repositories
     // https://api.github.com/users/android/repos
     // users/{user}/repos
-
+    @GET("users/{user}/repos")
     suspend fun getRepositories(
-        // TODO 11: Refer the variable to API path
-        user: String
+        @Path("user") user: String
     ): List<RepositoryResponse>
 }
